683fbd104d8b8acf0323e4c27c3a75cd1e446f5d
Marianecamposcordeiro <campos.cordeiro.mariane@escola.pr.gov.br>
2026-09-17 11:53:47 -0300

    ia

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
+  const perguntas = [
     {
-        enunciado: "O cyberbullying é a violência virtual que ocorre geralmente com as pessoas tímidas e indefesas, ou simplesmente por não caírem na simpatia das mesmas. Como podemos evitá-lo",
+        enunciado: "Em relação à preservação dos ecossistemas aquáticos, qual atitude garante a continuidade dos estoques de peixes e o equilíbrio da fauna?",
         alternativas: [
             {
-                texto: "Utilizando palestras para prevenir o cyberbullying nas escolas.",
-                afirmacao: "Você é uma pessoa reflexiva e busca aprendizado e conhecimento."
+                texto: "Qual é a sua abordagem principal em relação aos recursos pesqueiros ao planejar uma atividade à beira-rio ou no mar?",
+                afirmacao:"Pescar exclusivamente para consumo próprio, respeitando rigorosamente as cotas diárias e os tamanhos mínimos do peixe permitidos pela legislação.,"
+                "Optar por passeios de observação e ecoturismo náutico, sem a captura ou remoção de qualquer espécie do ecossistema."
             },
             {
-                texto:  "Prestando atenção aos sinais de cyberbullying nas escolas.",
-                afirmacao: "Você é uma pessoa observadora, pois se preocupa com o bem estar dos seus alunos."
+                texto:  "pescador Consciente: Valoriza o aproveitamento direto e sustentável da natureza para alimentação, mantendo a atividade estritamente alinhada com as regras de preservação ambiental..",
+                afirmacao: "Observador da Fauna: Prioriza o impacto zero no ecossistema aquático, preferindo a contemplação e o registro da biodiversidade em seu estado natural.."
             }    
            
         ]
@@ -39,7 +41,7 @@ const perguntas = [
             alternativas: [
                 {
                     texto: "Na maior parte do tempo, com certeza impacta, pois é a partir da autoestima que temos um olhar positivo ou negativo sobre nossas ações diárias. Sempre estou em busca de mais autoconhecimento.",
-                    afirmacao: "Você tem consciência que é importante ter uma boa auto-estima e procura se aprofundar mais sobre o assunto, vendo a vida de um jeito positivo."
+                    afirmacaoirmacao: "Você tem consciência que é importante ter uma boa auto-estima e procura se aprofundar mais sobre o assunto, vendo a vida de um jeito positivo."
                 },
                 {
                     texto:    "Apenas ocasionalmente ou raramente. Às vezes minha autoestima oscila e isso pode afetar negativamente minha qualidade de vida em certas ocasiões.",
@@ -49,41 +51,4 @@ const perguntas = [
                
             ]
         },
-]let atual = 0;
-let perguntaAtual;
-let historiaFinal = "";
-
-function mostraPergunta(){
-    if(atual >= perguntas.length){
-        mostraResultado();
-        return;
-    }
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = "";
-    mostraAlternativas();
-}
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild(botaoAlternativas);
-    }
-
-}
-function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
-    historiaFinal += afirmacoes + " ";
-    atual++
-    mostraPergunta();
-}
-function mostraResultado(){
-    caixaPerguntas.textContent = "Olha só o que podemos afirmar sobre você...";
-    textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = "";
-}
-
-
-
-mostraPergunta();
+]
